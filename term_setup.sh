f50d6b613d21296b5685873c015d021086fd66eb
AbdulHamid Merii <ah_merii@outlook.com>
2022-02-21 23:03:00 +0000

    Fix bugs

--- term_setup.sh
+++ term_setup.sh
@@ -1,39 +1,49 @@
-#!/bin/bash
+#!/bin/sh
+
+# checks if repo exists. if repo does not exist clones it else it pulls the most recent changes.
+git_clone_pull_repo() {
+    repo=$1 
+    repo_name=$(echo $repo | sed -rn 's/git@.+\/(.+)\.git/\1/p')
+    # handle empty input for directory
+    [ -z "$2" ] && directory="./" || directory=$2
+    # check if repo is already cloned or not; if exists then pull
+    if cd "$directory/$repo_name"; then git pull; else git clone --depth=1 $repo "$directory/$repo_name"; fi
+}
 
 sudo apt update
 sudo apt upgrade
 
 # install packages 
-sudo apt install \
+yes Y | sudo apt install \
+bat \
 zsh \
 unzip \
 jq \
-fontconfig \ 
-make \
-batcat
+fontconfig
 
 # download and install lsd (LSDeluxe)
 curl -Ls https://api.github.com/repos/Peltoche/lsd/releases/latest | grep -o https.*lsd_.*amd64.deb | wget -qi -
-dpkg -i lsd?*.deb
+sudo dpkg -i lsd?*.deb
 rm lsd?*.deb
 
-# change defualt shell to zsh
-chsh -s /bin/zsh
-
-source $HOME/.zshrc
-
-# create a directory for bash/zsh scripts and themes
-[! -d ~/plugins ] && mkdir ~/plugins
-[! -d ~/themes ] && mkdir ~/themes
-
 # install powerlevel 10k theme
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/themes
+git_clone_pull_repo git@github.com:romkatv/powerlevel10k.git ~/themes
 
 ## install plugins and scripts
-git clone --depth=1 git@github.com:zsh-users/zsh-syntax-highlighting.git ~/plugins/zsh-syntax-highlighting
-git clone --depth=1 git@github.com:MichaelAquilina/zsh-you-should-use.git ~/plugins/zsh-you-should-use
-git clone --depth=1 git@github.com:Tarrasch/zsh-command-not-found.git ~/plugins/zsh-command-not-found
-git clone --depth=1 git@github.com:zsh-users/zsh-autosuggestions.git ~/plugins/zsh-autosuggestions
+git_clone_pull_repo git@github.com:zsh-users/zsh-syntax-highlighting.git ~/plugins
+git_clone_pull_repo git@github.com:Tarrasch/zsh-command-not-found.git ~/plugins
+git_clone_pull_repo git@github.com:MichaelAquilina/zsh-you-should-use.git ~/plugins
+git_clone_pull_repo git@github.com:zsh-users/zsh-autosuggestions.git ~/plugins
+
+# create a directory for bash/zsh scripts and themes
+[ ! -d ~/plugins ] && mkdir ~/plugins # the use of the exclacamtion mark is not allowing the program to run need to figure out how to use the not operation!
+[ ! -d ~/themes ] && mkdir ~/themes
+
+# change defualt shell to zsh
+sudo chsh -s $(which zsh) $(whoami)
+
+
+exec /bin/zsh
 
 # installing miniconda
 mkdir -p ~/miniconda3
@@ -45,4 +55,3 @@ rm -rf ~/miniconda3/miniconda.sh
 
 # installing mamba
 conda install mamba -n base -c conda-forge
-
